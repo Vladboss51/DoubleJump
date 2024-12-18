98e56b31ca905e9aac822100f982641c6703cfad
Vladboss51 <vlad80920@gmail.com>
2024-12-18 23:37:42 +0300

    112233

--- Assets/Scripts/Camera.cs
+++ Assets/Scripts/Camera.cs
@@ -1,13 +1,13 @@
 using UnityEngine;
 
-public class Camera : MonoBehaviour
+public class Cam : MonoBehaviour
 {
     [SerializeField] private Player _player1;
     [SerializeField] private PlayerSecond _player2;
 
     private void FixedUpdate()
     {
-        transform.position = new Vector3(0, (_player1.transform.position.y + _player2.transform.position.y) / 2 + 7, -20f);
+        transform.position = new Vector3(0, (_player1.transform.position.y + _player2.transform.position.y) / 2 + 5, -20f);
 
     }
 }
